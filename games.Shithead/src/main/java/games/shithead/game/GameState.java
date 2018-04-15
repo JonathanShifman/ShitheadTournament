de40f1efa2c690a3b752e3d275adc07bdbb4780f
Jonathan <jonathan.shifman@gmail.com>
2018-04-15 23:48:50 +0300

    GameState class documentation

--- games.Shithead/src/main/java/games/shithead/game/GameState.java
+++ games.Shithead/src/main/java/games/shithead/game/GameState.java
@@ -12,44 +12,66 @@ import java.util.stream.Collectors;
 
 public class GameState {
 
+    static Logger logger = LogManager.getLogger(GameState.class);
+
     private Random rnd = new Random();
 
-    //True if the time for registration is over, and the participants have been determined
+    // True if the time for registration is over, and the participants have been determined
     private boolean isGameStarted;
 
-    //Maps player ids to their hands (the cards in their possession)
+    // Maps player ids to their hands (the cards in their possession)
     private Map<Integer, IPlayerHand> players;
 
+    // The multi deck that has been allocated for the game
     private IMultiDeck deck;
-    private CardStatus[] cardStatuses;
+
+    // Maps a card's unique id (as the array index) to itself
     private IGameCard[] cards;
+
+    // Maps a card's unique id (as the array index) to its status
+    private CardStatus[] cardStatuses;
+
+    // Used to allocate incrementing unique ids to cards as they are drawn from the deck
     private int cardUniqueIdAllocator = 0;
 
+    // The number of players who are yet to select their table cards
     private int playersPendingTableCardsSelection;
 
+    /* The id of the current move.
+     * Used to prevent ambiguity in case a PlayerActionMessage was delayed */
     private int currentMoveId = 1;
 
+    // The id of the player who performed the last accepted action
     private int lastPerformedActionPlayer = -1;
+
+    // Indicates whether or not the turn should be stolen by the player who performed the last move
     private boolean shouldAwardTurnToLastPerformedActionPlayer = false;
+
+    // Indicated whether or not the turn of the next player should be skipped
     private boolean shouldSkipOne = false;
 
+    // Holds the cards that are currently in the pile
     private List<IGameCard> pile;
 
-    //Queue of ids of players defining the order of their turns
+    // Queue of ids of players defining the order of their turns
     private Deque<Integer> playingQueue = new LinkedBlockingDeque<>();
+
+    // The id of the player whose turn it is to play next
     private int currentTurnPlayerId = -1;
 
+
+    // The number of hand cards a player should have at the start of the game
     private final int HAND_CARDS_AT_GAME_START = 3;
+
+    // The number of revealed table cards a player should have at the start of the game
     private final int REVEALED_TABLE_CARDS_AT_GAME_START = 3;
+
+    // The number of hidden table cards a player should have at the start of the game
     private final int HIDDEN_TABLE_CARDS_AT_GAME_START = 0;
 
     public GameState() {
         isGameStarted = false;
         players = new HashMap<>();
-    }
-
-    public boolean enoughPlayersToStartGame() {
-        return players.size() > 1;
     }
 
     public boolean isGameStarted() {
@@ -64,19 +86,57 @@ public class GameState {
         return players.size();
     }
 
+    public int getCurrentPlayerTurn() {
+        return currentTurnPlayerId;
+    }
+
+    public List<IGameCard> getPile() {
+        return pile;
+    }
+
+    public int getCurrentMoveId() {
+        return currentMoveId;
+    }
+
+    public int getRevealedCardsAtGameStart() {
+        return HAND_CARDS_AT_GAME_START;
+    }
+
+    private void incrementCurrentMoveId() {
+        currentMoveId++;
+    }
+
+    /**
+     * Adds a player to the game
+     * @param playerId The id of the new player
+     */
     public void addPlayer(int playerId) {
         PlayerHand playerHand = new PlayerHand();
         players.put(playerId, playerHand);
     }
 
+    /**
+     * Checks if there are enough players to start the game
+     * @return true if there are enough players, false otherwise
+     */
+    public boolean enoughPlayersToStartGame() {
+        return players.size() > 1;
+    }
+
+    /**
+     * Starts the game by initializing the required date
+     */
     public void startGame() {
         isGameStarted = true;
         pile = new ArrayList<>();
-        initDecks();
+        initDeck();
         dealInitialCards();
     }
 
-    private void initDecks() {
+    /**
+     * Initializes the game deck
+     */
+    private void initDeck() {
         //Try to match deck size to number of players
         System.out.println("Initializing deck");
         deck = new MultiDeck((int) Math.ceil((double)players.size()/4));
@@ -87,6 +147,10 @@ public class GameState {
         }
     }
 
+    /**
+     * Deals the players their initial cards, including cards that are pending
+     * players' selections (between being hand cards or revealed table cards)
+     */
     public void dealInitialCards() {
         int numOfCardsToDeal = HAND_CARDS_AT_GAME_START + REVEALED_TABLE_CARDS_AT_GAME_START +
                 HIDDEN_TABLE_CARDS_AT_GAME_START;
@@ -112,6 +176,12 @@ public class GameState {
         playersPendingTableCardsSelection = getNumberOfPlayers();
     }
 
+    /**
+     * Performs a selection of revealed table cards by a player.
+     * The non-selected cards will become the player's hand cards.
+     * @param playerId The id of the player making the selection
+     * @param selectedCardsIds The ids of the selected revealed table cards
+     */
     public void performTableCardsSelection(int playerId, List<Integer> selectedCardsIds) {
         if(playersPendingTableCardsSelection == 0) {
             throw new RuntimeException("Exception: All players already selected table cards");
@@ -149,12 +219,17 @@ public class GameState {
         playersPendingTableCardsSelection--;
     }
 
+    /**
+     * Checks if all players have selected their revealed table cards
+     * @return True if all players have made their selection, false otherwise
+     */
     public boolean allPlayersSelectedTableCards() {
         return playersPendingTableCardsSelection == 0;
     }
 
-
-
+    /**
+     * Produces a random permutation to represent the order of the players' turns
+     */
     private void determinePlayersOrder() {
         List<Integer> playerIds = new ArrayList<>(players.keySet());
         playerIds.sort((id1, id2) -> rnd.nextBoolean() ? 1 : rnd.nextBoolean() ? 0 : -1);
@@ -163,10 +238,20 @@ public class GameState {
         logger.info("Players order: " + playingQueue.toString());
     }
 
+    /**
+     * Starts the game cycle once all players have selected their revealed table cards
+     */
     public void startCycle() {
         determinePlayersOrder();
     }
 
+    /**
+     * Validates an attempted action by a player. Throws an exception in case of an invalid action.
+     * @param playerId The id of the player attempting the action
+     * @param cardsToPut The cards the player attempted to play. An empty list indicates the player is
+     *                   attempting to take the pile.
+     * @param moveId The id of the move this action is relevant for
+     */
     private void validateAction(int playerId, List<Integer> cardsToPut, int moveId) {
         logger.info("Attempting action: cards " + toCardDescriptions(cardsToPut) + " by player " + playerId);
         if(!players.containsKey(playerId)) {
@@ -192,6 +277,15 @@ public class GameState {
         }
     }
 
+    /**
+     * Performs an action attempted by a player, as long as it is valid.
+     * @param playerId The id of the player attempting the action
+     * @param cardsToPut The cards the player attempted to play. An empty list indicates the player is
+     **                  attempting to take the pile.
+     * @param moveId The id of the move this action is relevant for
+     * @param victimId The id of the victim chosen to take the pile in case a joker was played.
+     *                 Will be ignored if a joker wasn't played.
+     */
     public void performPlayerAction(int playerId, List<Integer> cardsToPut, int moveId, int victimId) {
         validateAction(playerId, cardsToPut, moveId);
         logger.info("Performing action");
@@ -216,25 +310,15 @@ public class GameState {
         }
         lastPerformedActionPlayer = playerId;
         updatePlayerTurn();
-        updateCurrentMoveId();
+        incrementCurrentMoveId();
     }
 
-    public int getCurrentMoveId() {
-        return currentMoveId;
-    }
-
-    private void updateCurrentMoveId() {
-        currentMoveId++;
-    }
-
-    private String toCardDescriptions(List<Integer> cardsToPut) {
-        String cardValues = "";
-        for(int cardId : cardsToPut) {
-            cardValues += cards[cardId].getCardFace().get().getValue() + ", ";
-        }
-        return "[" + cardValues + "]";
-    }
-
+    /**
+     * Updates the necessary information for implementation of special effects when
+     * a special card has been played.
+     * @param victimId The id of the victim chosen to take the pile - Only relevant if
+     *                 a joker was played.
+     */
     private void updateSpecialEffects(int victimId) {
         if(completedFour()) {
             shouldAwardTurnToLastPerformedActionPlayer = true;
@@ -287,6 +371,11 @@ public class GameState {
         }
     }
 
+    /**
+     * Checks if the latest move completed a set of 4 cards with the same value
+     * at the top of the pile.
+     * @return True is a set of 4 has been completed, false otherwise.
+     */
     private boolean completedFour() {
         if(pile.size() < 4) {
             return false;
@@ -310,6 +399,10 @@ public class GameState {
 
     }
 
+    /**
+     * Marks the given cards as burned
+     * @param cardsToBurn The cards to burn
+     */
     private void burnCards(List<IGameCard> cardsToBurn) {
         logger.info("Burning cards");
         for(IGameCard gameCard : cardsToBurn) {
@@ -317,6 +410,10 @@ public class GameState {
         }
     }
 
+    /**
+     * Updates the turn to the player who is supposed to play next, with consideration
+     * to special effects
+     */
     private void updatePlayerTurn() {
         if(shouldAwardTurnToLastPerformedActionPlayer) {
             shouldAwardTurnToLastPerformedActionPlayer = false;
@@ -331,17 +428,28 @@ public class GameState {
         advancePlayingQueue();
     }
 
+    /**
+     * Advances the playing queue by 1 step
+     */
     private void advancePlayingQueue() {
         playingQueue.addLast(playingQueue.poll());
         currentTurnPlayerId = playingQueue.getFirst();
     }
 
+    /**
+     * Advances the playing queue to the selected player
+     * @param currentTurnPlayerId The id of the player to advance the queue to
+     */
     private void advancePlayingQueue(int currentTurnPlayerId) {
         while (playingQueue.getFirst() != currentTurnPlayerId) {
             playingQueue.addLast(playingQueue.poll());
         }
     }
 
+    /**
+     * Deals a player as many cards as needed (if at all) to complete his hand
+     * @param playerId The id of the player to deal cards to
+     */
     private void dealPlayerCardsIfNeeded(int playerId) {
         IPlayerHand playerInfo = players.get(playerId);
         int neededCards = 3 - playerInfo.getHandCards().size();
@@ -357,6 +465,10 @@ public class GameState {
         }
     }
 
+    /**
+     * Checks if the game is over
+     * @return True if the game is over, false otherwise
+     */
     public boolean isGameOver() {
         if(!deck.isEmpty()) {
             return false;
@@ -371,15 +483,16 @@ public class GameState {
         return false;
     }
 
-    public int getCurrentPlayerTurn() {
-        return currentTurnPlayerId;
-    }
-
-    public List<IGameCard> getPile() {
-        return pile;
-    }
-
-    public int getRevealedCardsAtGameStart() {
-        return HAND_CARDS_AT_GAME_START;
+    /**
+     * Converts a list of cards to a string made of their values. Used for logging.
+     * @param cardsToPut The list of cards to analyze
+     * @return A String representing the sequence of the given cards' values
+     */
+    private String toCardDescriptions(List<Integer> cardsToPut) {
+        String cardValues = "";
+        for(int cardId : cardsToPut) {
+            cardValues += cards[cardId].getCardFace().get().getValue() + ", ";
+        }
+        return "[" + cardValues + "]";
     }
 }
