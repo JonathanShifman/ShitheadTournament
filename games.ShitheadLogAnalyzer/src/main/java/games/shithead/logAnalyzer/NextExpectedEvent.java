bbbf48e8bde638e3ea184115ea2663631d72128f
Jonathan <jonathan.shifman@gmail.com>
2018-06-12 00:26:24 +0300

    Updated log analyzer

---
+++ games.ShitheadLogAnalyzer/src/main/java/games/shithead/logAnalyzer/NextExpectedEvent.java
@@ -0,0 +1,11 @@
+package games.shithead.logAnalyzer;
+
+public enum NextExpectedEvent {
+    GAME_START,
+    CYCLE_START,
+    PERFORMING_ACTION,
+    GAME_STATE,
+    PLAYER_STATE,
+    PILE,
+    MOVE_ID
+}
